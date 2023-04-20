73ff3ca377e4989b5dffe1a302b6b01a2c5500c8
Viljo Holma <viljo.holma@gmail.com>
2023-04-20 12:14:06 +0300

    api testings

--- app/src/main/java/com/findworker/findworker/MainActivity.java
+++ app/src/main/java/com/findworker/findworker/MainActivity.java
@@ -7,12 +7,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,9 +42,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void makeSearch() {
-        TextView results = findViewById(R.id.results);
 
-        String url = "http://localhost:8000/backend/search/";
+        /*TextView results = (TextView) findViewById(R.id.results);
+
+        String url = "http://10.0.2.2:8000/backend/search/";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -54,7 +60,44 @@ public class MainActivity extends AppCompatActivity {
         });
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(jsonObjectRequest);
+        requestQueue.add(jsonObjectRequest);*/
+
+        final TextView textView = (TextView) findViewById(R.id.results);
+        // ...
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://10.0.2.2:8000/backend/search/";
+
+        // Request a string response from the provided URL.
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Display the first 500 characters of the response string.
+                        System.out.println(response);
+                        try {
+                            String testi = response.get(1).toString();
+                            System.out.println(testi);
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                        try {
+                            textView.setText("Response is: " + response.get(0).toString());
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonArrayRequest);
+
 
 
 
