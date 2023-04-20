ef363df2e33a68c4b2c92f68f59a4c5001d01611
Viljo Holma <viljo.holma@gmail.com>
2023-04-20 10:31:00 +0300

    button/api testailuja

--- app/src/main/java/com/findworker/findworker/LogInActivity.java
+++ app/src/main/java/com/findworker/findworker/LogInActivity.java
@@ -2,13 +2,38 @@ package com.findworker.findworker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
+    private Button logInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        logInButton = findViewById(R.id.logInButton);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToMainAppActivity();
+            }
+        });
     }
+
+    public void goToMainAppActivity() {
+
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+    }
+
+
+
+
 }
--- app/src/main/java/com/findworker/findworker/MainActivity.java
+++ app/src/main/java/com/findworker/findworker/MainActivity.java
@@ -3,15 +3,61 @@ package com.findworker.findworker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchButton = findViewById(R.id.searchButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeSearch();
+            }
+        });
+
+
         //testaus
+    }
+
+    private void makeSearch() {
+        TextView results = findViewById(R.id.results);
+
+        String url = "http://localhost:8000/backend/search/";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                results.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                results.setText("error");
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+
+
+
     }
 
 
